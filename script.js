62b88ef7adfbb93b14857ed0a0544aeb8c826e61
okborges <rafael.okborges@gmail.com>
2024-07-28 19:14:57 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+function armazenarValores() {
+  const formulario = document.getElementById("meuFormulario");
+  const valores = [];
+  const respostas = [
+    "strength",
+    "washin'",
+    "squeeze",
+    "ass",
+    "homie",
+    "link up",
+    "apple",
+    "lie",
+    "dine",
+    "grind",
+  ];
+
+  for (let i = 1; i <= 10; i++) {
+    const input = formulario["input" + i];
+    valores.push(input.value);
+  }
+
+  let total = 0;
+  for (let i = 0; i < valores.length; i++) {
+    if (valores[i].toLowerCase().trim() === respostas[i].toLowerCase().trim()) {
+      total++;
+    }
+  }
+
+  Toastify({
+    text: `Você acertou ${total} palavras`,
+    duration: -1,
+    gravity: "top",
+    close: true,
+    position: "center",
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      background: "linear-gradient(to right, #00b09b, #96c93d)",
+    },
+  }).showToast();
+}
